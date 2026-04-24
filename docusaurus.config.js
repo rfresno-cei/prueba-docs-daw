a595ac4d84fd47b624f067ed23874cc28f0c7b05
rfresno-cei <rafaelfresno@profesorado.cei.es>
2026-04-24 11:21:24 +0200

    Added domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://prueba-docs-daw.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
